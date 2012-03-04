21e4240561d35c03e76686dc0c4f08467ac24197
Ali Ayoubi <aloush89@gmail.com>
2012-03-04 18:42:54 +0100

    Added support for config files

--- src/bot.py
+++ src/bot.py
@@ -5,18 +5,35 @@
 import twitter 
 import time
 import sys
+from xml.etree import ElementTree
 
 #local import
 import bitly
 from grabber import get_last_post
 
-API = twitter.Api(consumer_key='XXX',
-                  consumer_secret='XXX',
-                  access_token_key='XXX',
-                  access_token_secret='XXX')
+doc = ElementTree.parse('twitter-auth.xml')
+try:
+        c_key = doc.find('ConsumerKey').text
+        c_secret = doc.find('ConsumerSecret').text
+        a_key = doc.find('AccessKey').text
+        a_secret = doc.find('AccessSecret').text
+except:
+        print "Error in XML file: twitter-auth.xml. Tag not found"
 
-BAPI= bitly.Api(login='XXX',
-		apikey='XXX')
+API = twitter.Api(consumer_key=c_key,
+                  consumer_secret=c_secret,
+                  access_token_key=a_key,
+                  access_token_secret=a_secret)
+
+doc = ElementTree.parse('bitly-auth.xml')
+try:
+        login= doc.find('Login').text
+        apikey= doc.find('Apikey').text
+except:
+        print "Error in XML file bitly-auth.xml. Tag not found"
+
+BAPI= bitly.Api(login=login,
+		apikey=apikey)
 
 def compose_message(post):
 	'''
@@ -30,6 +47,8 @@ def post_message(message, title):
 	'''
 	post tweet if length is correct
 	'''
+	print "Message to post: %s" % (message)
+	print ""
 	if len(message) > 141:
 		print("Message is too long...")
 		return False
@@ -42,33 +61,43 @@ def shorten_url(url):
 	'''
 	shorten url using bit.ly's api
 	'''
+	print "Shortening URL: %s" % (url)
 	short_url = BAPI.shorten(url)
 	return short_url
+
+def update_file_post(post):
+        with open('.lastpost', 'w') as f:
+                f.write(str(post.get('id')))
+
+def get_file_post():
+        with open('.lastpost', 'r') as f:
+                postid = f.readline()
+        return postid
 
 def main():
 	'''
 	Main program 
 	'''
 	post_count=0
-	current = get_last_post()
-	if current == None:
-		print("Error getting last post...")
-		sys.exit(1)
-	message = compose_message(current)
-        result = post_message(message, current.get('title'))
 	while True:
+		try:
+			current_post_id = int(get_file_post())
+		except ValueError:
+			print "Error in .lastpost"
+			sys.exit(1)
 		last_post = get_last_post()
 		if last_post == None:
 			time.sleep(60)
 			continue
-		if last_post.get('id') == current.get('id'):
+		if last_post.get('id') == current_post_id:
 			time.sleep(60)
 			continue
-		current = last_post
+		print "Compared current post with id: %d to previous id: %d. Posting message."  % (current_post_id, last_post.get('id'))
 		message = compose_message(last_post)
 		result = post_message(message, last_post.get('title'))
+		update_file_post(last_post)
 		if result:
-			post_count+=1
+			print "Posted successfully!"
 
 if __name__=='__main__':
 	main()
