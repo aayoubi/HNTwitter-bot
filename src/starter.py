3dcf6db9dd3894839aef9bd2d2803ebc80738ae8
Ali Ayoubi <aloush89@gmail.com>
2012-03-04 18:45:30 +0100

    starter script, to be executed before bot.py

---
+++ src/starter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/eval PYTHONPATH=/home/lma21/python-modules/ python
+# -*- coding: utf-8 -*-
+
+from grabber import get_last_post
+import sys
+
+def update_file(post):
+	with open('.lastpost', 'w') as f:
+		f.write(str(post.get('id')))
+
+def get_post():
+	with open('.lastpost', 'r') as f:
+                postid = f.readline()
+	return postid
+
+def main():
+        '''
+        Main program
+        '''
+        post = get_last_post()
+        if post == None:
+                print("Error getting last post...")
+                sys.exit(1)
+
+	update_file(post)
+
+if __name__=='__main__':
+        main()
+
